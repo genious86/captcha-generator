1b6ef32cdee04134e678b86cb431c08dbc550d79
genious86 <pauleugenius86@gmail.com>
2024-09-18 15:56:10 +0300

    declaring variables

---
+++ index.js
@@ -0,0 +1,7 @@
+const display = document.getElementById("the_status")
+const body = document.body;
+const submit = document.getElementById("_submit");
+const refresh = document.getElementById("_refresh");
+
+const char = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
