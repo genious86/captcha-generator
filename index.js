6509108a04879e92b3dadb04f5b411100386b428
genious86 <pauleugenius86@gmail.com>
2024-09-18 16:13:58 +0300

    function generatig captcha and var determining length

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ const refresh = document.getElementById("_refresh");
 
 const char = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
+let captcha = "";
+
+
+body.onload = function generateCaptcha(){
+
+    const captchaLength = 6;
+    for(let i = 0; i<captchaLength; i++){
+        const randomIndex = Math.floor(Math.random() * char.length);
+        captcha += char.substring(randomIndex,randomIndex + 1);
+    }
+
+    document.getElementById("_generator").value = captcha;
+    display.innerText = "Captcha Generator"
+
+
+}
